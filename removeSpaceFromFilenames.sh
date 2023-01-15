f59aff884c3a092a5d4491e09c75ba943e0468e9
David Li <fantasticmrdavid@gmail.com>
2023-01-16 09:29:05 +1100

    Add removeSpacesFromFilenames.sh

---
+++ removeSpaceFromFilenames.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for file in *' '*
+do
+  if [ -e "${file// /_}" ]
+  then
+    printf >&2 '%s\n' "Warning, skipping $file as the renamed version already exists"
+    continue
+  fi
+
+  mv -- "$file" "${file// /_}"
+done
